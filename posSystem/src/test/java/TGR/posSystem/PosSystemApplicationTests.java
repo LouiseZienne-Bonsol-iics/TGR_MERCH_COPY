32cd1982a2826fff238b5ce20db35382a365e5cf
KiraAkigari <87720175+KiraAkigari@users.noreply.github.com>
2022-04-02 19:28:01 +0800

    The Start of Something New
    Backend lmao

---
+++ posSystem/src/test/java/TGR/posSystem/PosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package TGR.posSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
