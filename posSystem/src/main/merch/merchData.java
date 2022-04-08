12512cf34d66f085ee0a3bb294d4aba8395eed1e
KiraAkigari <87720175+KiraAkigari@users.noreply.github.com>
2022-04-08 13:14:04 +0800

    MerchData

--- posSystem/src/main/merch/merchData.java
+++ posSystem/src/main/merch/merchData.java
@@ -1,4 +1,4 @@
-package merch;
+package posSystem.src.main.merch;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,21 +10,20 @@ public class merchData {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-
-    private String title;
-    private String content;
+    private String OrderID;
+    private String LastName;
+    private String FirstName;
+    private String MerchType;
+    private float MerchPrice;
+    private String OrderStatus;
 
     public merchData() {  }
 
-    public merchData(String title, String content) {
-        this.setTitle(title);
-        this.setContent(content);
-    }
 
-    public merchData(int id, String title, String content) {
+    public merchData(int id, String OrderID, String LastName, String FirstName, String MerchType, float MerchPrice, String OrderStatus) {
         this.setId(id);
-        this.setTitle(title);
-        this.setContent(content);
+        this.setOrderID(OrderID);
+        this.setLastName(LastName);
     }
 
     public int getId() {
@@ -35,28 +34,57 @@ public class merchData {
         this.id = id;
     }
 
-    public String getTitle() {
-        return title;
+    public String getOrderID() {
+        return OrderID;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setOrderID(String OrderID) {
+        this.OrderID = OrderID;
     }
 
-    public String getContent() {
-        return content;
+    public String getLastName() {
+        return LastName;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getMerchType() {
+        return MerchType;
+    }
+
+    public void setMerchType(String MerchType) {
+        this.MerchType = MerchType;
+    }
+
+    public float getMerchPrice() {
+        return MerchPrice;
+    }
+
+    public void setMerchPrice(String MerchPrice) {
+        this.MerchPrice = MerchPrice;
+    }
+
+    public String getOrderStatus() {
+        return OrderStatus;
+    }
+
+    public void setOrderStatus(String OrderStatus) {
+        this.OrderStatus = OrderStatus;
     }
 
     @Override
     public String toString() {
-        return "merchData{" +
-                "id=" + id +
-                ", title='" + title + ''' +
-                ", content='" + content + ''' +
-                '}';
+
+        return "merchData{" + "id=" + id + ", OrderID='" + OrderID + "' , LastName='" + LastName + "',FirstName= '" + FirstName + "', MerchType='" + MerchType + "', MerchPrice='" + MerchPrice + "', OrderStatus='" + OrderStatus + "'}";
     }
 }
