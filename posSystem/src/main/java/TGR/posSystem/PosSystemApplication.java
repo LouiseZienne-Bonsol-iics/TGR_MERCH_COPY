ee051297c5cd6a7b3f04daa7c3916dc439dcc6bb
KiraAkigari <87720175+KiraAkigari@users.noreply.github.com>
2022-04-11 22:10:14 +0800

    test

--- posSystem/src/main/java/TGR/posSystem/PosSystemApplication.java
+++ posSystem/src/main/java/TGR/posSystem/PosSystemApplication.java
@@ -1,13 +1,24 @@
 package TGR.posSystem;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class PosSystemApplication {
+public class PosSystemApplication implements CommandLineRunner{
 
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
 	public static void main(String[] args) {
 		SpringApplication.run(PosSystemApplication.class, args);
 	}
-
+	@Override
+	public void run(String... args) throws Exception 
+	{
+		String sql = "SELECT * FROM ?";
+         
+        jdbcTemplate.update(sql, "");
+	}
 }
