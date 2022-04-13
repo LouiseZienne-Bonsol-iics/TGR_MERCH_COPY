76fe95748aea753d8ac7053ee40049aaf298c1fc
KiraAkigari <87720175+KiraAkigari@users.noreply.github.com>
2022-04-14 02:47:50 +0800

    Update loginController.java

--- posSystem/src/main/java/tgr/controller/loginController.java
+++ posSystem/src/main/java/tgr/controller/loginController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -36,7 +37,7 @@ public class loginController
  
     @PostMapping("/login")
     //public String login(@ModelAttribute("user") login user ) {
-    public String login(@ModelAttribute login login, BindingResult result, Model model){
+    public String login(@RequestBody login login){
     
 
      login oauthUser = userService.login(login.getUsername(), login.getPassword());
