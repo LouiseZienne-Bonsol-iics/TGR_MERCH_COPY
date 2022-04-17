fb051a2ae6d9f8d3fc6b54b44c0fff25ac9be203
KiraAkigari <87720175+KiraAkigari@users.noreply.github.com>
2022-04-17 21:45:23 +0800

    Update PosSystemApplication.java

--- posSystem/src/main/java/tgr/PosSystemApplication.java
+++ posSystem/src/main/java/tgr/PosSystemApplication.java
@@ -1,24 +1,14 @@
 package tgr;
 
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-//import order;
 
 
 @SpringBootApplication
 public class PosSystemApplication
-{
-	@Autowired
-	JdbcTemplate jdbcTemplate;
-	public static void main(String[] args) {
+{	public static void main(String[] args) {
 		SpringApplication.run(PosSystemApplication.class, args);
 	}
 }
