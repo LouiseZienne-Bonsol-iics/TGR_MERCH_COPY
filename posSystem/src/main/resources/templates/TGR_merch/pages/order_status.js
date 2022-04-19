dde78e503f867b40bb598d037d957fef9b5426af
KiraAkigari <87720175+KiraAkigari@users.noreply.github.com>
2022-04-19 20:33:53 +0800

    data

--- posSystem/src/main/resources/templates/TGR_merch/pages/order_status.js
+++ posSystem/src/main/resources/templates/TGR_merch/pages/order_status.js
@@ -5,48 +5,30 @@ var btnGetWeather = document.getElementById('tracking-button');
 
 // Add Event Listener
 btnGetWeather.addEventListener('click', function() {
-    const name= "",contactNo= "",email= "",delivery= "",orderNo= "",orderDate= "",paymentstat= "",waybill= "",deliverystatus = "";
+    //const name= "",contactNo= "",email= "",delivery= "",orderNo= "",orderDate= "",paymentstat= "",waybill= "",deliverystatus = "";
   if (iptSearch.value === "52BIRX") 
   {
-    name = document.getElementById("customer-name");
-    name.innerHTML = "John Johnson";
-    contactNo = document.getElementById("contact-number");
-    contactNo.innerHTML = "091234567890";
-    email = document.getElementById("email-address");
-    email.innerHTML = "john.johnson4536@yahoo.com";
-    delivery = document.getElementById("delivery-option");
-    delivery.innerHTML = "JTS";
-    orderNo = document.getElementById("order-number");
-    orderNo.innerHTML("52BIRX");
-    orderDate = document.getElementById("order-date");
-    orderDate.innerHTML("04/12/2022");
-    paymentstat = document.getElementById("payment");
-    paymentstat.innerHTML("Completed");
-    waybill = document.getElementById("waybill-no");
-    waybill.innerHTML("210-984395");
-    deliverystatus = document.getElementById("delivery-status");
-    deliverystatus.innerHTML("Delivering");
+    document.getElementById("customer-name").textContent = "John Johnson";
+    document.getElementById("contact-number").textContent= "091234567890";
+    document.getElementById("email-address").textContent="john.johnson4536@yahoo.com";
+    document.getElementById("delivery-option").textContent = "JTS";
+    document.getElementById("order-number").textContent="52BIRX";
+    document.getElementById("order-date").textContent="04/12/2022";
+    document.getElementById("payment").textContent="Completed";
+    document.getElementById("waybill-no").textContent="210-984395";
+    document.getElementById("delivery-status").textContent="Delivering";
   }
   if (iptSearch.value === "YCHYCQ")
   {
-    name = document.getElementById("customer-name");
-    name.innerHTML = "Steve Stevenson";
-    contactNo = document.getElementById("contact-number");
-    contactNo.innerHTML = "09198765432";
-    email = document.getElementById("email-address");
-    email.innerHTML = "steve.stevenson1985@yahoo.com";
-    delivery = document.getElementById("delivery-option");
-    delivery.innerHTML = "Lazada";
-    orderNo = document.getElementById("order-number");
-    orderNo.innerHTML("YCHYCQ");
-    orderDate = document.getElementById("order-date");
-    orderDate.innerHTML("04/15/2022");
-    paymentstat = document.getElementById("payment");
-    paymentstat.innerHTML("Completed");
-    waybill = document.getElementById("waybill-no");
-    waybill.innerHTML("219-150647");
-    deliverystatus = document.getElementById("delivery-status");
-    deliverystatus.innerHTML("Received");
+    document.getElementById("customer-name").textContent= "Steve Stevenson";
+    document.getElementById("contact-number").textContent = "09198765432";
+    document.getElementById("email-address").textContent = "steve.stevenson1985@yahoo.com";
+    document.getElementById("delivery-option").textContent = "Lazada";
+    document.getElementById("order-number").textContent="YCHYCQ";
+    document.getElementById("order-date").textContent="04/15/2022";
+    document.getElementById("payment").textContent="Completed";
+    document.getElementById("waybill-no").textContent="219-150647";
+    document.getElementById("delivery-status").textContent="Received";
   }
   else {
     console.log("City name isn't valid. Try again");
